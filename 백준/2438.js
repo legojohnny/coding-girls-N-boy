b80662f8876eb7bec44ec9de043af954a9146b82
echo-youn <tmdwo0727@naver.com>
2022-03-06 22:17:11 +0900

    test

--- 백준/2438.js
+++ 백준/2438.js
@@ -1,3 +1,30 @@
-// https://www.acmicpc.net/problem/2438
-// node 2438
+// https://www.acmicpc.net/problem/2438, 별 찍기 - 1
+// node 2438 1시간, 2시간
+// 20분 이상 고민해보시고... 답지보시거나, 일요일에 오셔서
+// 어떻게 접근하는가?
+// 변수로 저장을 해놓는다.
+// 2중 for문으로 루프를 돌린다.
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+rl.on('line', function (a) {
+
+    /**
+     * 백준 컴퓨터가 a에다가 변수 넣어줌.
+     * 아래에 코드를 작성하세요.
+     */
+
+    // 코드 작성, 매개변수 a
+
+    /**
+     * 여기까지 작성 영역
+     */
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
+
+// 구현능력 길러주는 훈련
--- 백준/2439.js
+++ 백준/2439.js
@@ -1,2 +1,2 @@
-// https://www.acmicpc.net/problem/2439
+// https://www.acmicpc.net/problem/2439, 별 찍기 - 2
 // node 2439
