2285b1906200956389d0cc5f11806e2c7866d4f5
legojohnny <99404732+legojohnny@users.noreply.github.com>
2022-03-16 19:19:08 +0900

    Update 2442.js

--- 백준/2442.js
+++ 백준/2442.js
@@ -1,2 +1,28 @@
 // https://www.acmicpc.net/problem/2442, 별 찍기 -5
 // node 2442
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (a) {
+    let star='';
+    let empty='';
+   
+   for(let i=1; i <= a; i++){
+    star = '*';
+    result = star.repeat((i*2)-1);
+    for(let j = 0;  j < a - i; j++){
+        empty += ' ';
+      }
+
+      console.log(empty + result);
+      empty = '';
+   };
+
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
