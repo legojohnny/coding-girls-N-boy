4e05dfb69158d6460d02468d7b081e1c84028ccd
legojohnny <99404732+legojohnny@users.noreply.github.com>
2022-03-16 19:18:51 +0900

    Update 2441.js

--- 백준/2441.js
+++ 백준/2441.js
@@ -1,2 +1,28 @@
 // https://www.acmicpc.net/problem/2441, 별 찍기 - 4
 // node 2441
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (a) {
+    let star='';
+    let empty='';
+   
+   for(let i=0; i <= a-1; i++){
+    empty += ' ';
+    for(let j = 0;  j < a - i; j++){
+        star += '*';
+      }
+      result = empty + star;
+      result2= result.substr(1, a);
+      console.log(result2);
+      star = '';
+   };
+    
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
