14bf07ebeeeb318e673a2beab01855252b37e4f1
legojohnny <99404732+legojohnny@users.noreply.github.com>
2022-03-16 19:18:10 +0900

    Update 2439.js

--- 백준/2439.js
+++ 백준/2439.js
@@ -1,2 +1,25 @@
 // https://www.acmicpc.net/problem/2439, 별 찍기 - 2
 // node 2439
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (a) {
+    let star='';
+    let empty='';
+   
+   for(let i=1; i <= a; i++){
+    star += '*';
+    for(let j = 0;  j < a - i; j++){
+        empty += ' ';
+      }
+      console.log(empty + star);
+      empty = '';
+   };
+
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
