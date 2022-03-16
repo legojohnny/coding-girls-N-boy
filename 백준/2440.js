4b04188e48e890e738068cb266ad88a1f80ed0b8
legojohnny <99404732+legojohnny@users.noreply.github.com>
2022-03-16 19:18:31 +0900

    Update 2440.js

--- 백준/2440.js
+++ 백준/2440.js
@@ -1,2 +1,21 @@
 // https://www.acmicpc.net/problem/2440, 별 찍기 - 3
 // node 2440
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', function (a) {
+
+    let star = '*';
+      for(let i=a; i >= 1; i--){
+          console.log(star.repeat(i));
+      }
+
+
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
